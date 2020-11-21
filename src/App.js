d103b2eb46860259d37a0f3a52822adfff38e816
Karolina Maciejewska <kmaciejewska14@gmail.com>
2020-11-21 14:08:34 +0100

    Added code to distinguish between different users' groups

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+import _ from 'lodash';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const [isClient, updateClientInfo] = useState(false)
+  const [isStaff, updateStaffInfo] = useState(false)
+  const [isAdmin, updateAdminInfo] = useState(false)
+  useEffect(() => {
+    /* Get the AWS credentials for the current user from Identity Pools.  */
+    Auth.currentSession()
+      .then(cognitoUser => {
+        const { idToken: { payload }} = cognitoUser
+        /* Loop through the groups that the user is a member of */
+        /* Set isClient to true if the user is part of the Clients group */
+        payload['cognito:groups'] && payload['cognito:groups'].forEach(group => {
+          if (group === 'Clients') updateClientInfo(true)
+          else if (group === 'Staff')   updateStaffInfo(true)
+          else if (group === 'Admins') updateAdminInfo(true)
+        })
+      })
+      .catch(err => console.log(err));
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        { isClient && <h1>Welcome, Client!</h1> }
+        { isStaff && <h1>Welcome, Staff!</h1> }
+        { isAdmin && <h1>Welcome, Admin!</h1> }
+      </header>
+    </div>
+  );
 }
 
 export default withAuthenticator(App, true);
