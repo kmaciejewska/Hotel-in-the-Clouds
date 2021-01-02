a1023fc6851ecdba52880b7fa83bcf8c23a142df
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-01-02 13:57:18 +0100

    implementation of createBooking and processPayment, grapql schema

--- amplify/backend/function/createBooking/src/index.js
+++ amplify/backend/function/createBooking/src/index.js
@@ -1,14 +1,81 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const BOOKING_TABLE = "<booking_table_name>";
+const BOOKING_TYPE = "Order";
+const ROOM_BOOKING_TABLE = "<room_booking_table name>";
+const ROOM_BOOKING_TYPE = "RoomBooking";
 
 
+const createBooking = async (payload) => {
+  const { booking_id, username, email, dateFrom, dateTo, total } = payload;
+  var params = {
+    TableName: BOOKING_TABLE,
+    Item: {
+      id: booking_id,
+      __typename: BOOKING_TYPE,
+      customer: email,
+      user: username,
+      dateFrom: dateFrom,
+      dateTo: dateTo,
+      total: total,
+      updatedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString()
+    }
+  };
+  console.log(params);
+  await documentClient.put(params).promise();
+};
+
+const createRoomBooking = async (payload) => {
+  let roomBookings = [];
+  for (i = 0; i < payload.cart.length; i++) {
+    const cartItem = payload.cart[i];
+    roomBookings.push({
+      PutRequest: {
+        Item: {
+          id: uuidv4(),
+          __typename: ROOM_BOOKING_TYPE,
+          room_id: cartItem.id,
+          booking_id: payload.booking_id,
+          customer: payload.email,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString()
+        }
+      }
+    });
+  }
+  let params = {
+    RequestItems: {}
+  };
+  params["RequestItems"][ROOM_BOOKING_TABLE] = roomBookings;
+  console.log(params);
+  await documentClient.batchWrite(params).promise();
+};
+
+/*
+ * Get booking details from processPayment lambda
+ * Create a booking
+ * Link rooms to the booking - Users can see the past bookings and admins can view bookings by user
+ * Email the invoice (Will be added later)
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  try {
+    let payload = event.prev.result;
+    payload.booking_id = uuidv4();
+
+    // create a new booking
+    await createBooking(payload);
+
+    // links rooms with the booking
+    await createRoomBooking(payload);
+
+    // Note - function to email the invoice to the user
+
+    return "SUCCESS";
+  } catch (err) {
+    console.log(err);
+    return new Error(err);
+  }
 };
