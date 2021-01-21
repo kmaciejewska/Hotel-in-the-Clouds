64a7f14fa2c3f6f8d4590f172e700a1170498bae
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2021-01-21 15:15:48 +0100

    Calendar bookings read from file

--- HotelManagerSpace/resort/src/components/Scheduler.js
+++ HotelManagerSpace/resort/src/components/Scheduler.js
@@ -3,51 +3,19 @@ import { Inject, ScheduleComponent, Month, TimelineViews, TimelineMonth, Week, D
     Resize, ResourcesDirective, ResourceDirective, ViewsDirective, ViewDirective} from "@syncfusion/ej2-react-schedule";
 import Title from "./Title";
 
+import { scheduleData } from '../scheduleData';
+import { extend } from '@syncfusion/ej2-base';
+
 class Scheduler extends Component {
 
     constructor(props) {
         super(props);
+        this.data = extend([], scheduleData, null, true);
+
         this.state = {
     
             localData:  {
-                dataSource: [{
-                    Id: 1,
-                    EndTime: new Date(2020, 10, 14, 6, 30),
-                    StartTime: new Date(2020, 10, 11, 4, 0),
-                    Subject: 'Reservation - not paid',
-                    IsAllDay: true,
-                    ResourceID: 1,
-                    GroupID: 1
-                },
-                {
-                    Id: 2,
-                    EndTime: new Date(2020, 10, 5, 6, 30),
-                    StartTime: new Date(2020, 10, 3, 4, 0),
-                    Subject: 'Reservation - not paid',
-                    IsAllDay: true,
-                    ResourceID: 2,
-                    GroupID: 6
-                },
-                {
-                    Id: 3,
-                    EndTime: new Date(2020, 10, 15, 6, 30),
-                    StartTime: new Date(2020, 10, 7, 4, 0),
-                    Subject: 'Reservation - not paid',
-                    IsAllDay: true,
-                    ResourceID: 2,
-                    GroupID: 7
-                },
-
-                {
-                    Id: 3,
-                    EndTime: new Date(2020, 10, 15, 6, 30),
-                    StartTime: new Date(2020, 10, 7, 4, 0),
-                    Subject: 'Reservation - not paid',
-                    IsAllDay: true,
-                    ResourceID: 4,
-                    GroupID: 5
-                }
-                ]
+                dataSource: [{}]
             },
 
             localResourceData: {
@@ -91,7 +59,7 @@ class Scheduler extends Component {
                 <Title title="Room availability"/>
                 <div className="component">
                     <ScheduleComponent className="hotelRooms" startResizable={false} currentView='TimelinMonth' selectedDate={new Date(2020, 10, 5, 6, 30)}
-                    eventSettings={this.state.localData}
+                    eventSettings={{ dataSource: this.data }}
                     group={this.state.localGroupData}>
                         <ResourcesDirective>
                             <ResourceDirective className="resourceStyle" field="ResourceID" title="Resource Name" name="Resources"
---
+++ HotelManagerSpace/resort/src/scheduleData.js
@@ -0,0 +1,42 @@
+/**
+ * Schedule datasource
+ */
+
+export let scheduleData = [ 
+    {   
+        Id: 1,
+        EndTime: new Date(2020, 10, 14, 6, 30),
+        StartTime: new Date(2020, 10, 11, 4, 0),
+        Subject: 'Reservation - not paid',
+        IsAllDay: true,
+        ResourceID: 1,
+        GroupID: 1
+    }, 
+    {
+        Id: 2,
+        EndTime: new Date(2020, 10, 5, 6, 30),
+        StartTime: new Date(2020, 10, 3, 4, 0),
+        Subject: 'Reservation - not paid',
+        IsAllDay: true,
+        ResourceID: 2,
+        GroupID: 6
+    }, 
+    {
+        Id: 3,
+        EndTime: new Date(2020, 10, 15, 6, 30),
+        StartTime: new Date(2020, 10, 7, 4, 0),
+        Subject: 'Reservation - not paid',
+        IsAllDay: true,
+        ResourceID: 2,
+        GroupID: 7
+    }, 
+    {
+        Id: 3,
+        EndTime: new Date(2020, 10, 15, 6, 30),
+        StartTime: new Date(2020, 10, 7, 4, 0),
+        Subject: 'Reservation - not paid',
+        IsAllDay: true,
+        ResourceID: 4,
+        GroupID: 5
+    }
+];
